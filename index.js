1c88099fcc4cf1689dee4309a8d9a0378e8dec1a
Jaime7w7r <jaimeadolfov07@gmail.com>
2023-03-03 17:55:04 -0600

    Paginas

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function main() {
-    document.getElementById("main").innerHTML = "<div id='pag2'><div class='container'><div><h1 style='float: left;'>Fenrir</h1><img src='media/ico.ico'></div><h1 class='title animated bounceInLeft'>Bienvenidos al Juego de Aventuras</h1><p class='subtitle animated slideInRight'>Este juego es para niños y está lleno de aventuras y desafíos.</p><button id='button1' class='btn btn-primary' onclick='cap()'>Capturar alias</button><div class='canvas-container'><canvas id='gameCanvas'></canvas></div><img src='media/uaa.png' alt='logo' class='logo' width='10%'></div>";
+    document.getElementById("main").innerHTML = "<div id='pag2'><div class='container'><div><h1 style='float: left;'>Fenrir</h1><img src='media/ico.ico'></div><h1 class='title animated bounceInLeft'>Bienvenidos al Juego de Aventuras</h1><p class='subtitle animated slideInRight'>Este juego es para niños y está lleno de aventuras y desafíos.</p><button id='button1' class='btn btn-primary' onclick='cap()'>Capturar alias</button><button id='button1' class='btn btn-primary' onclick='cre()'>Creditos</button><div class='canvas-container'><canvas id='gameCanvas'></canvas></div><img src='media/uaa.png' alt='logo' class='logo' width='10%'></div>";
     var canvas = document.getElementById("gameCanvas");
     var ctx = canvas.getContext("2d");
     canvas.width = window.innerWidth;
@@ -26,40 +26,21 @@ function main() {
 }
 
 function cap() {
-    document.getElementById("main").innerHTML = "<div class='ejemplo'><br><label for='nombre'> Alias: </label><input type='text' id='nombre' /><input type='button' id='boton' onclick='verificar()' value='Verificar datos' /><p id='datos'> Pulsa para ver tu nombre y tu password. </p></div>"
+    document.getElementById("main").innerHTML = "<input type='button' id='boton' onclick='main()' value='Volver al menu' /><div class='ejemplo'><br><label for='nombre'> Alias: </label><input type='text' id='nombre' /><input type='button' id='boton' onclick='verificar()' value='Verificar datos' /><p id='datos'> Pulsa para ver tus datos. </p></div>"
 }
 
-function alta(i) {
-    var carr = JSON.stringify(
-        {
-            descripcion: Name[i],
-            precio: Precio[i]
-        });
-    carrito.push(carr);
-    localStorage.setItem("carrito", JSON.stringify(carrito));
-    lista();
-}
-
-function Eliminar(i) {
-    document.getElementById("liveAlertPlaceholder").innerHTML = " ";
-    var carr = JSON.parse(carrito[i]);
-    var descripcion = carr.descripcion;
-    carrito.splice(i, 1);
-    localStorage.setItem("carrito", JSON.stringify(carrito));
-    lista();
-    var alertPlaceholder = document.getElementById('liveAlertPlaceholder');
-    var wrapper = document.createElement('div');
-    wrapper.innerHTML = '<div class="alert alert-succes alert-dismissible" role="alert">' + carr.descripcion + ' Eliminado<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>';
-    alertPlaceholder.append(wrapper);
+function cre(){
+    document.getElementById("main").innerHTML = "<input type='button' id='boton' onclick='main()' value='Volver al menu' /><div class='ejemplo'><h1 class='title animated bounceInLeft'>Creditos</h1><br><label> Leonardo Javier Hernandez Martinez </label><p id='datos'> ID 245439. </p><br><label> Alfredo Quezada Márquez (Papi) </label><p id='datos'> ID 274258. </p><br><label> Bárbara Jimena Silva Jaime </label><p id='datos'> ID 244923. </p><br><label> Jaime Adolfo Varela Martínez </label><p id='datos'> ID 295482. </p></div>"
 }
 
 function verificar() {
     if (localStorage.nombre != undefined) {
-        document.getElementById("datos").innerHTML = "Nombre: " + localStorage.nombre;
+        document.getElementById("datos").innerHTML = "Alias: " + localStorage.alias+"Puntuación: "+localStorage.puntuacion+"Tiempo: "+localStorage.tiempo;
     }
     else {
-        document.getElementById("datos").innerHTML = "No has introducido tu nombre y tu password";
+        document.getElementById("datos").innerHTML = "No se encuentran datos";
     }
+    document.getElementById("main").innerHTML += "<input type='button' style='margin-left: 45%;' id='boton' onclick='El juego xd' value='Empezar el juego' />";
 }
 
 window.onload = main;
